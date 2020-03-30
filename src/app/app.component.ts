dfce4d46f2af47d9e9e70a86e9db26962cd59ece
holanrahmatullah <holan1700016004@webmail.uad.ac.id>
2020-03-30 23:26:20 +0700

    navigasi

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './halaman-utama.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tekweb1700016004';
+}
+
